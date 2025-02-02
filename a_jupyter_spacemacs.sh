8ce38458428ff2fdf3258d43be4333865e9e24db
Paul T <4277512+pault-tech@users.noreply.github.com>
2025-02-02 02:55:38 +0000

    wip

--- a_jupyter_spacemacs.sh
+++ a_jupyter_spacemacs.sh
@@ -7,5 +7,14 @@ pip install --user jupytext #sync ipynb .py files; required for
 echo TERM=xterm-256color jupyter notebook #for ipython-notebook
 
 jupytext --to py:percent --output aa.py mynotebook.ipynb #eg command
+ls *.py
+
+# unexpected indent FIX
+ipython profile create default
+# warn(('Ignoring {0} in favour of {1}. Remove {0} to '
+# [ProfileCreate] Generating default config file: PosixPath('/home/ubuntu/.ipython/profile_default/ipython_config.py')
+# [ProfileCreate] Generating default config file: PosixPath('/home/ubuntu/.ipython/profile_default/ipython_kernel_config.py')
+echo "c.TerminalInteractiveShell.autoindent=False" >> ~/.ipython/profile_default/ipython_config.py
+
 
 #use (ein:notebooklist-open)
