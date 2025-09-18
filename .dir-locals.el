31f78bd55d62dc5ebafca616ee8282cd72c30e27
Paul T <4277512+pault-tech@users.noreply.github.com>
2025-09-18 06:59:50 +0000

    fix

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 (
  (python-mode . ((mysubmit-build-cmd . "a_build.sh")
-                 (fssubmit-build-cmd . "a_build.sh")
+                 (fssubmit-build-cmd . "a_build.sh"))
  )
  ;; (conda-project-env-path "pytorch_env")
  (conda-project-env-path "/home/ubuntu/anaconda3/envs/pytorch3")
