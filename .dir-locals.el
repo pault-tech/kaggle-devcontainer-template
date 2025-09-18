55064134824d402fc85aa4e3b653b5cc52af6212
Paul T <4277512+pault-tech@users.noreply.github.com>
2025-09-18 01:09:28 +0000

    add dir-locals and build file

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+(
+ (python-mode . ((mysubmit-build-cmd . "a_build.sh")
+                 (fssubmit-build-cmd . "a_build.sh")
+ )
+ ;; (conda-project-env-path "pytorch_env")
+ (conda-project-env-path "/home/ubuntu/anaconda3/envs/pytorch3")
+ )
---
+++ a_build.sh
@@ -0,0 +1,110 @@
+i
+# TODO: merge this with working project versions
+
+echo a_build.sh $0 $1 $2
+echo filename arg $1
+echo optional dobatch arg $2
+
+# exit
+
+cd /workspaces/_proj-dir
+homedir="/home/_todo_"
+symlnk="_todo_"
+
+
+
+function kaggle_push_kernel {
+
+bn=`basename $1`
+fname="${bn%.*}"
+echo name is $fname
+
+#
+# 4. push kernel
+# Kernel push error: Notebook not found
+# NOTE above error if notebook was not saved at least once
+tm="60" kernelname="_kernel-gnn-p1" #NOTE: underscore and uppercase are invalid chars. eg helloWorld_01 is invalid
+tm="120" kernelname="$fname" #NOTE: underscore and uppercase are invalid chars. eg helloWorld_01 is invalid
+echo kaggle kernels push --path ./$kernelname
+cd $kernelname
+pwd
+printf "\n\n\n"
+kaggle kernels push --path ./
+printf "\n"
+cd ..
+# echo $homedir/bin/winutils.sh kag_mon > /tmp/execstr_kag.sh $tm
+echo "$homedir/bin/winutils.sh kag_mon $tm $kernelname" > /tmp/execstr_kag.sh
+date
+# https://github.com/Kaggle/kaggle-api/issues/575
+# Notebook not found (make sure saved at least 1 version of notebook)
+}
+# kaggle_push_kernel $1
+# date
+
+function push_to_gpu {
+    
+# TODO: use gnu screen on gpu instance for long running cmds
+scp -i ~/e104*pem fname/fname_p4.ipynb  ec2-user@gpu:~/
+ssh -i ~/e104*pem  ec2-user@gpu \
+'source activate pytorch && jupyter nbconvert --to notebook --execute fname_p4.ipynb --output=new.ipynb'
+
+
+}
+# push_to_gpu
+# date
+
+
+
+
+function local_jupyter_batch {
+
+#NOTE if needed install papermill 
+# echo eval "$(conda shell.bash hook)"
+# echo conda activate python3
+# echo pip install nbconvert # for batch submission of notebooks
+# echo pip install papermill # for batch submission of notebooks
+echo      conda install -y --name python3 -c conda-forge papermill nbconvert
+
+bn=`basename $1`
+fname="${bn%.*}"
+echo name is $fname
+
+# source ~/.bashrc; source ../.env; conda activate python3; echo $CONDA_PREFIX; type ipython
+eval "$(conda shell.bash hook)"
+conda activate python3
+
+cd /workspaces/_proj-dir/$fname
+set -x
+kernelname="_kernel-tts-p1"
+kernelname="_kernel-llm"
+tm="60" kernelname="$fname" #NOTE: underscore and uppercase are invalid chars. eg helloWorld_01 is invalid
+papermill $kernelname.ipynb $kernelname.batchrun.ipynb --log-output -k python3
+
+# kernelname="_kernel-llm"
+jupyter nbconvert --to html $kernelname.batchrun.ipynb
+
+# kernelpath="/workspaces/_proj-dir/$fname/$kernelname.batchrun.ipynb"
+# echo "scp spot01:/workspaces/_proj-dir/$fname/$fname* $homedir/$symlnk/Downloads/;  explorer.exe c:/; " > /tmp/execstr_kag.sh
+# echo "scp spot01:/workspaces/_proj-dir/$fname/$fname*html $homedir/$symlnk/Downloads/;  explorer.exe c:/; " > /tmp/execstr_kag.sh
+echo "scp spot01:/workspaces/_proj-dir/$fname/$fname*html $homedir/$symlnk/Downloads/;  explorer.exe c:/; t 3 '{ctrl+w}{alt+tab}{ctrl+2}r'; " > /tmp/execstr_kag.sh
+# sleep 3; ~/bin/t 0 '{ctrl+w}' 
+
+set +x
+echo done
+date
+
+
+}
+# local_jupyter_batch $1
+# date
+
+# echo optional dobatch arg $2
+if [ "$2" = "dobatch" ]; then
+    local_jupyter_batch $1
+    date
+else
+    kaggle_push_kernel $1
+    date
+fi
+
+
